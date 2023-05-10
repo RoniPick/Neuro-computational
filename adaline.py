ae6b930526a3f2ea22f246226df03702a8b5dedd
RoniPick <92802335+RoniPick@users.noreply.github.com>
2023-05-10 15:54:44 +0300

    fixed Adeline functions

--- adaline.py
+++ adaline.py
@@ -12,21 +12,43 @@ class Adaline:
     def fit(self, X, y):
         n_features = X.shape[1]
         self.weights = np.random.rand(n_features + 1)
-        # self.errors = []
+        self.errors = []
         for epoch in range(self.epochs):
-            error_sum = 0.0
-            for i in range(len(X)):
-                activation = self.activation(np.dot(self.weights[1:], X) + self.weights[0])
-                errors = y - activation
-                self.weights[0] += self.learning_rate * errors.sum()
-                self.weights[1:] += self.learning_rate * np.dot(X.T, errors)
-                error_sum += errors ** 2
-            self.errors.append(error_sum)
-            if error_sum == 0:
-                break
+            activation = self.activation(np.dot(X, self.weights[1:]) + self.weights[0])
+            errors = y - activation
+            self.weights[0] += self.learning_rate * errors.sum()
+            self.weights[1:] += self.learning_rate * np.dot(X.T, errors)
+            self.errors.append(errors.sum())
 
     def activation(self, X):
         return X
 
     def predict(self, X):
-        return np.where(self.activation(np.dot(self.weights[1:], X) + self.weights[0]) >= 0, 1, -1)
+        return np.where(self.activation(np.dot(X, self.weights[1:]) + self.weights[0]) >= 0, 1, -1)
+
+    def accuracy(self, X, y):
+        predictions = self.predict(X)
+        correct = np.sum(predictions == y)
+        return correct / len(y)
+
+    # def fit(self, X, y, tolerance=1e-4):
+    #     n_features = X.shape[1]
+    #     self.weights = np.random.rand(n_features + 1).astype(float)
+    #     prev_error = np.inf
+    #
+    #     for epoch in range(self.epochs):
+    #         error_sum = 0.0
+    #         for i in range(len(X)):
+    #             activation = self.activation(
+    #                 np.dot(self.weights[1:].astype(float), X[i]).astype(float) + self.weights[0].astype(float))
+    #             errors = y[i] - activation
+    #             self.weights[0] += self.learning_rate * errors
+    #             self.weights[1:] += self.learning_rate * X[i] * errors
+    #             error_sum += errors ** 2
+    #
+    #         self.errors.append(error_sum)
+    #
+    #         if abs(prev_error - error_sum) < tolerance:
+    #             break
+    #
+    #         prev_error = error_sum
--- main.py
+++ main.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
                     line = line.replace(' ', '')
                     line = line.replace('(', '')
                     line = line.replace(')', '')
+                    line = line.replace('\n', '')
 
                     line = line.split(',')
 
@@ -42,21 +43,32 @@ if __name__ == '__main__':
                             letter_mem.append(line[1:])
 
     # Convert the letters list to a NumPy array
-    letter_mem = np.array(letter_mem)
-    letter_bet = np.array(letter_bet)
-    letter_lamed = np.array(letter_lamed)
+    letter_mem = np.array(letter_mem, dtype=int)
+    letter_bet = np.array(letter_bet, dtype=int)
+    letter_lamed = np.array(letter_lamed, dtype=int)
+
+    label_mem = [1] * len(letter_mem)
+    label_bet = [-1] * len(letter_bet)
 
     # Combine labels and letters for "bet" and "mem" categories
     labels_combined = np.concatenate((label_bet, label_mem))
     letters_combined = np.concatenate((letter_bet, letter_mem))
 
+    print("labels_combined: ", labels_combined)
+
+    # print("labels_combined: ", labels_combined)
+    # print("letters_combined: ", letters_combined)
+
     # Shuffle the data
     combined_data = np.column_stack((labels_combined, letters_combined))
     np.random.shuffle(combined_data)
+    # print("combined_data: ", combined_data)
 
     # Split the shuffled data back into labels and letters
     shuffled_labels = combined_data[:, 0]
     shuffled_letters = combined_data[:, 1:]
+    # print("shuffled_labels: ", shuffled_labels)
+    # print("shuffled_letters: ", shuffled_letters)
 
     # Split data into training and test sets
     train_ratio = 0.8  # Percentage of data for training
